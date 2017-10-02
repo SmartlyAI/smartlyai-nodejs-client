06082f4febcd87611ea3a1ac2248455130bb8fbe
Pascal Giammelluca <xxalbireo@gmail.com>
2017-10-02 10:05:49 +0200

    Change PLATFORM to 'api'

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const AUTH_URI = 'https://apis.smartly.ai/api/auth/oauth/token'
 const DIALOG_URI = 'https://apis.smartly.ai/api/dialog/'
 const NEW_SESSION_EV = 'NEW_DIALOG_SESSION'
 const NEW_INPUT_EV = 'NEW_INPUT'
-const PLATFORM = 'smartlyai-nodejs-client'
+const PLATFORM = 'api'
 
 class Dialog {
   init({ grant_type='password', password, username, client_id, client_secret }) {
